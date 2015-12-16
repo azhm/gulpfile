1449237ad1e6794b71b6599d32cf162381537d5f
储涛 <chutao@socialpark.com.cn>
2015-12-16 17:37:31 +0800

    gulp 初始配置

---
+++ app/assets/js/app.js
@@ -0,0 +1,15 @@
+$(function () {
+    var _tap = ("ontouchstart" in window) ? "tap" : "click";
+    new mo.Landscape();
+    new mo.PCPrompt({
+        url: '',
+        title: '',
+        description: ' ',
+        preview: 'preview.jpg',
+        prefix: 'pc',
+        jump: function () {
+            alert('请用移动端扫一扫二维码，进行浏览。');
+            $("#pc_jump").hide();
+        }
+    });
+});
