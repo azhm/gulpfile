2eea81205a493d98afceb495dd288fb58e8e18f1
储涛 <chutao@socialpark.com.cn>
2016-05-18 10:28:15 +0800

    gulp 配置文件

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -1,18 +1,5 @@
 $(function () {
     var _tap = ("ontouchstart" in window) ? "tap" : "click";
-    new mo.Landscape();
-    new mo.PCPrompt({
-        url: '',
-        title: '',
-        description: ' ',
-        preview: '',
-        prefix: 'pc',
-        jump: function () {
-            alert('请用移动端扫一扫二维码，进行浏览。');
-            $("#pc_jump").hide();
-        }
-    });
-
-    //tp.tongji(");
-
+   
+    //tp.tongji("");
 });
